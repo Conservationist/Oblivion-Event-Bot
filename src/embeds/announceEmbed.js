a96fe10352d66af588ee15f5ed52a870b92dfc20
Hexybu <conorosullivanmitchell@gmail.com>
2018-08-05 20:04:43 +0300

    Some minor visual updates.

--- src/embeds/announceEmbed.js
+++ src/embeds/announceEmbed.js
@@ -4,7 +4,7 @@ export default function announceEmbed(title, id) {
     const embed = new Discord.RichEmbed()
         .setColor("#d0ef84")
         .setAuthor(`📆 New event listed.`)
-        .setDescription(`New event **${title}**(\`${id}\`) has been listed, go sign-up!`)
+        .setDescription(`New event **${title}** (\`${id}\`) has been listed, go sign-up!`)
         .setFooter(
             "Relative of the Billy Herrington bot. | Made by 𝓗𝓮𝔁.",
             "https://cdn.discordapp.com/avatars/469204371289145345/561ea91258cf5ccad4685a1b4f52355e.png?size=2048"
